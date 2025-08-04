d67b6bee2d87547beef796e8bb65e1a1d44d7e1b
Peter <peter86tsai@gmail.com>
2025-08-04 15:48:15 +0800

    上傳headers改為"text/plain", 修改UI

--- docs/assets/js/line_messaging.js
+++ docs/assets/js/line_messaging.js
@@ -5,37 +5,54 @@ const options = {
         return {
             liffId: appLiffId,
             scriptUrl: appScriptUrl,
+            userId: "",
+            status: "初始化中...",
         };
     },
     methods: {
         async getUser() {
             const liffId = this.liffId;
 
-            await liff.init({ liffId });
-            if (!liff.isLoggedIn()) {
-                liff.login();
-                console.log("liff尚未登入");
-                return;
+            try {
+                await liff.init({ liffId });
+                if (!liff.isLoggedIn()) {
+                    liff.login();
+                    console.log("liff尚未登入");
+                    return;
+                }
+
+                const profile = await liff.getProfile();
+                const userId = profile.userId;
+
+                this.status = "初始化完成";
+                this.userId = userId;
+
+                await fetch(this.scriptUrl, {
+                    method: "POST",
+                    headers: { "Content-Type": "text/plain" },
+                    body: JSON.stringify({ userId }),
+                })
+                    .then((res) => {
+                        return res.text();
+                    })
+                    .then((msg) => {
+                        console.log(`GAS 回應: ${msg}`);
+                    })
+                    .catch((err) => {
+                        console.error(`傳送失敗: ${err}`);
+                    });
+
+                // fetch(`${this.scriptUrl}?userId=${userId}`)
+                //     .then((res) => {
+                //         return res.text();
+                //     })
+                //     .then((text) => {
+                //         alert(text);
+                //     });
+            } catch (e) {
+                this.status = "初始化錯誤";
+                console.error(e);
             }
-
-            const profile = await liff.getProfile();
-            const userId = profile.userId;
-
-            console.log(userId);
-
-            await fetch(this.scriptUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ userId }),
-            });
-
-            // fetch(`${this.scriptUrl}?userId=${userId}`)
-            //     .then((res) => {
-            //         return res.text();
-            //     })
-            //     .then((text) => {
-            //         alert(text);
-            //     });
         },
     },
     mounted() {
